5e9e19e6fc9067bf4facb2f021a816ce35a57e9e
alexdrid <alexdrid13@gmail.com>
2020-09-19 13:59:32 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Lady Gaga</h1>
+      <p>It wasn't love it was a perfect illusion</p>
     </div>
   );
 }
