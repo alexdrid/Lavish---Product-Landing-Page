f45715277b628e5aa5a6556b4f80777917c1200c
alexdrid <alexdrid13@gmail.com>
2020-09-19 15:17:13 +0300

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Lady Gaga</h1>
-      <p>It wasn't love it was a perfect illusion</p>
+      <Navbar />
     </div>
   );
 }
