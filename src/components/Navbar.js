f6d08692acba588d2e8842ff5477045975157f59
alexdrid <alexdrid13@gmail.com>
2020-09-19 16:18:30 +0300

    4 files modified, 1 added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { MdFingerprint } from "react-icons/md";
+
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [isClicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
   return (
-    <div className="header-container">
-      <header>Lavish</header>
+    <div className="navbar">
+      <div className="navbar-container container">
+        <Link to="/" className="navbar-logo">
+          <MdFingerprint className="navbar-icon" />
+          Lavish
+        </Link>
+        <div className="nav-menu-container">
+          <div className="menu-icon" onClick={handleClick}>
+            {isClicked ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={isClicked ? "nav-menu active" : "nav-menu"}>
+            <li className="menu-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
